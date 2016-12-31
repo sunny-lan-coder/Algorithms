c202325bf1b5489a51a858649be7b15020adfc51
sunny <sunny.lan.coder@gmail.com>
2016-12-31 12:35:01 -0500

    Finished ccc 2010 rip java

--- src/sigproc/FourierEasy.java
+++ src/sigproc/FourierEasy.java
@@ -16,7 +16,7 @@ public class FourierEasy {
 		}
 	}
 
-	static ArrayList<Sine> fft(int samples, double[] amplitudes) {
+	static ArrayList<Sine> dft(int samples, double[] amplitudes) {
 		ArrayList<Sine> res = new ArrayList<>();
 		for (int i = 0; i < samples; i++) {
 			for (int j = 0; j < samples; j++) {
@@ -28,10 +28,50 @@ public class FourierEasy {
 		return res;
 	}
 
+	static final double epsilon = 1e-10;
+
+	static double[][] dft(double[] data) {
+		int n = data.length;
+		double[][] res = new double[2][n];
+		for (int freq = 0; freq < n; freq++) {
+			double real = 0;
+			double imaginary = 0;
+
+			for (int sample = 0; sample < n; sample++) {
+				
+				
+				double phase = (2 * Math.PI)/n * freq * sample;
+
+				real += data[sample]*Math.cos(phase);
+				imaginary +=data[sample]* Math.sin(phase);
+			}
+
+			if (Math.abs(real) < epsilon)
+				real = 0;
+
+			if (Math.abs(imaginary) < epsilon)
+				imaginary = 0;
+
+			real /= n;
+			imaginary /= n;
+
+			res[0][freq] = real;
+			res[1][freq] = imaginary*Math.PI*2;
+
+		}
+		return res;
+	}
+
 	public static void main(String[] args) {
-		ArrayList<Sine> res = fft(4, new double[] { 0, 1, 0, 0 });
+		System.out.println("dft1");
+		ArrayList<Sine> res = dft(4, new double[] { 0, 1, 0, 0 });
 		for (Sine s : res) {
 			System.out.println(s.freq + ":" + s.phase + ":" + s.amp);
+		}
+		System.out.println("dft2");
+		double[][] res2=dft(new double[]{0,1,0,0});
+		for(int i=0;i<res2[0].length;i++){
+			System.out.println(i+":"+Math.toDegrees(res2[1][i])+":"+res2[0][i]);
 		}
 	}
 
